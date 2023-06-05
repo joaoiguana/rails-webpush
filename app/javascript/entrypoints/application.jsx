ff3b6ac8d3c174e2fd95cdded3d148313294a41b
joao garcia <jiboiadamor@gmail.com>
2023-06-05 12:43:55 +0100

    created a rails 7 vite web app to practice webpush notifications

---
+++ app/javascript/entrypoints/application.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from '../components/App';
+
+console.log('Vite ⚡️ Rails - Webpush');
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App />
+  </>
+);
